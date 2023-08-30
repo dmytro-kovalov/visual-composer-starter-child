566cad6f983d57655b685430ac475f2eec6127ea
Dmytro Kovalov <chop.suet@gmail.com>
2023-08-30 15:33:58 +0200

    Add ajax actions for testing purposes

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @package visual-composer-starter-child
  */
 
-add_action( 'wp_enqueue_scripts', 'visual_composer_starter_parent_theme_enqueue_styles' );
+
 
 /**
  * Enqueue scripts and styles.
@@ -19,3 +19,29 @@ function visual_composer_starter_parent_theme_enqueue_styles() {
 		[ 'visual-composer-starter-style' ]
 	);
 }
+
+add_action( 'wp_enqueue_scripts', 'visual_composer_starter_parent_theme_enqueue_styles' );
+
+/**
+ * Enqueue test scripts
+ */
+function visual_composer_starter_child_enqueue_scripts() {
+    wp_localize_script('jquery', 'vcsc', array(
+		'ajaxurl' => admin_url('admin-ajax.php'), 
+		'nonce' => wp_create_nonce('vcsc_ajax_nonce')
+	));
+}
+
+add_action('wp_enqueue_scripts', 'visual_composer_starter_child_enqueue_scripts');
+
+function visual_composer_starter_child_ajax_callback() {
+    if (isset($_POST['nonce']) && wp_verify_nonce($_POST['nonce'], 'vcsc_ajax_nonce')) {
+        echo "hellooo";
+    } else {
+        echo "Invalid nonce.";
+    }
+    wp_die();
+}
+
+add_action('wp_ajax_nopriv_vcsc_test_ajax', 'visual_composer_starter_child_ajax_callback');
+add_action('wp_ajax_vcsc_test_ajax', 'visual_composer_starter_child_ajax_callback');
